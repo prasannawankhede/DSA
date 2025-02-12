17b3fdfe7d1c11269aa2ef39c3a7c9ca00e92378
prasanna <prasannawankhede100@gmail.com>
2025-02-12 12:19:42 +0530

    for multiple answers

--- src/arrayProblems/TwoSum.php
+++ src/arrayProblems/TwoSum.php
@@ -1,31 +1,28 @@
 <?php
+
 namespace App\arrayProblems;
 
 class TwoSum
 {
-
     public function sum($arr, $target)
     {
-
         $n = count($arr);
 
         if ($n < 2) {
             return -1;
         }
 
+        // Traverse each unique pair (i, j) with j > i.
         for ($i = 0; $i < $n; $i++) {
-
             for ($j = $i + 1; $j < $n; $j++) {
-
                 if ($arr[$i] + $arr[$j] == $target) {
-
+                    // Return the first valid pair as a flat array.
                     return [$i, $j];
-
                 }
             }
         }
 
+        // If no pair is found, return -1.
         return -1;
-
     }
 }
--- tests/arrayProblems/TwoSumTest.php
+++ tests/arrayProblems/TwoSumTest.php
@@ -7,7 +7,6 @@ use App\arrayProblems\TwoSum;
 
 class TwoSumTest extends TestCase {
 
-    // Test when a valid pair is found.
     public function testSumFound() {
         $obj = new TwoSum();
         $arr = [2, 7, 11, 15];
@@ -15,7 +14,6 @@ class TwoSumTest extends TestCase {
         $this->assertEquals([0, 1], $result);
     }
     
-    // Test when no valid pair is found.
     public function testSumNotFound() {
         $obj = new TwoSum();
         $arr = [1, 2, 3, 4];
@@ -23,30 +21,50 @@ class TwoSumTest extends TestCase {
         $this->assertEquals(-1, $result);
     }
     
-    // Test with duplicate numbers.
     public function testSumWithDuplicates() {
         $obj = new TwoSum();
-        // There are two pairs that sum to 6: (2,4) and (4,2), but our method should return the first encountered pair.
+        // In this array, the first valid pair encountered will be [0,1] (2 + 4 = 6)
         $arr = [2, 4, 4, 1];
         $result = $obj->sum($arr, 6);
-        // Since the inner loop starts at $i+1, the first valid pair is [0,1] (2+4=6).
         $this->assertEquals([0, 1], $result);
     }
     
-    // Test with negative numbers.
     public function testSumWithNegativeNumbers() {
         $obj = new TwoSum();
         $arr = [-3, 4, 3, 90];
         $result = $obj->sum($arr, 0);
-        // -3 + 3 = 0, so expected indices are [0, 2]
+        // Expected: -3 + 3 = 0, so indices [0,2]
         $this->assertEquals([0, 2], $result);
     }
     
-    // Test with an array of length less than 2.
     public function testSingleElementArray() {
         $obj = new TwoSum();
         $arr = [5];
         $result = $obj->sum($arr, 5);
         $this->assertEquals(-1, $result);
+    }
+    
+    /**
+     * Test case that accepts multiple valid answers.
+     *
+     * In this example, multiple pairs could sum to the target.
+     * Depending on the iteration order, a valid answer could be [0,1] or [0,3] or [2,3].
+     * Our current algorithm will return the first valid pair it encounters.
+     */
+    public function testMultipleValidAnswers() {
+        $obj = new TwoSum();
+        // This array is set up so that there are multiple pairs that sum to 7.
+        // Possible valid pairs (with indices, assuming j > i) are:
+        //   - When i=0: if arr[0] = 3 and arr[?]=4, or
+        //   - When i=2: if arr[2] = 3 and arr[3] = 4.
+        // We will accept either [0,1] or [2,3] as valid.
+        $arr = [3, 4, 3, 4];
+        $result = $obj->sum($arr, 7);
+        $validAnswers = [
+            [0, 1],
+            [2, 3]
+        ];
+        // Assert that the result is one of the valid pairs.
+        $this->assertContains($result, $validAnswers);
     }
 }
