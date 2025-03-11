87603f8bd9137303809bc695cb6e238695484eda
prasanna <prasannawankhede100@gmail.com>
2025-03-11 19:52:34 +0530

    goooooo

--- src/arrayProblems/CheckIfArraySorted.php
+++ src/arrayProblems/CheckIfArraySorted.php
@@ -1,26 +1,22 @@
 <?php
-
 namespace App\ArrayProblems;
 
-class CheckIfArraySorted{
+class CheckIfArraySorted
+{
 
-    public function check(array $arr):bool{
-
+    public function check(array $arr): bool
+    {
 
         if (count($arr) <= 1) {
             return true;
         }
 
+        for ($i = 1; $i < count($arr); $i++) {
 
+            if ($arr[$i] < $arr[$i - 1]) {
 
-        for($i = 1; $i < count($arr); $i++){
-
-                
-                if($arr[$i] < $arr[$i - 1]){
-
-                    return false;
-                }
-            
+                return false;
+            }
         }
         return true;
     }
