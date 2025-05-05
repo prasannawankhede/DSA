bcd7f9e8060aec460ad7ff99ad00a49140f29f35
prasanna <prasannawankhede100@gmail.com>
2025-05-05 12:45:39 +0530

    go

--- tests/slidingWindow/MinSumSubarrayTest.php
+++ tests/slidingWindow/MinSumSubarrayTest.php
@@ -8,10 +8,10 @@ class MinSumSubarrayTest extends TestCase
     {
         $minSumSubarray = new MinSumSubarray();
 
-        $this->assertEquals(3, $minSumSubarray->minSum([1, 2, 3, 4, 5], 2));    // 1+2
-        $this->assertEquals(7, $minSumSubarray->minSum([2, 1, 5, 1, 3, 2], 3)); // 1+3+2
-        $this->assertEquals(5, $minSumSubarray->minSum([2, 3, 4, 1, 5], 2));    // 4+1
-        $this->assertEquals(0, $minSumSubarray->minSum([], 3));                 // empty array
-        $this->assertEquals(0, $minSumSubarray->minSum([1, 2], 3));             // window size too big
+        $this->assertEquals(3, $minSumSubarray->minSum([1, 2, 3, 4, 5], 2)); // 1+2
+        $this->assertEquals(6, $minSumSubarray->minSum([2, 1, 5, 1, 3, 2], 3));
+        $this->assertEquals(5, $minSumSubarray->minSum([2, 3, 4, 1, 5], 2)); // 4+1
+        $this->assertEquals(0, $minSumSubarray->minSum([], 3));              // empty array
+        $this->assertEquals(0, $minSumSubarray->minSum([1, 2], 3));          // window size too big
     }
 }
